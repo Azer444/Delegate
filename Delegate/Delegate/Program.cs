b81fa50804cc81dc7c0471ba03ab94b7ef5e27eb
Azer444 <Azerrh@code.edu.az>
2022-07-13 00:20:14 +0400

    Delegate

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegate
+{
+    class Program
+    {
+        //public delegate bool CheckNums(int num);
+
+        //static void Main(string[] args)
+        //{
+        //    //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        //    //Console.WriteLine(Sum(CheckOdd, arr));
+        //    //Console.WriteLine(Sum(CheckEven, arr));
+        //    //Console.WriteLine(Sum(CheckSumMoreFive, arr));
+        //    List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //    var result = numbers.FindAll(m=>m>6);
+
+
+        //}
+
+
+
+
+
+
+        //public static bool CheckOdd(int number)
+        //{
+        //    return number % 2 != 0;
+        //}
+
+        //public static bool CheckEven(int number)
+        //{
+        //    return number % 2 == 0;
+        //}
+
+        //public static bool CheckSumMoreFive(int number)
+        //{
+        //    return number > 6;
+        //}
+
+
+
+
+
+        //public static int Sum(Predicate<int> predicate, int[] nums)
+        //{
+        //    var sum = 0;
+
+        //    foreach (var item in nums)
+        //    {
+        //        if (predicate(item))
+        //        {
+        //            sum += item;
+        //        }
+        //    }
+        //    return sum;
+
+
+
+        //}
+
+        //public static int SumEven(CheckNums fact, int[] nums)
+        //{
+        //    var sum = 0;
+
+        //    foreach (var item in nums)
+        //    {
+        //        if (fact(item))
+        //        {
+        //            sum += item;
+        //        }
+        //    }
+        //    return sum;
+
+
+
+
+
+            //}
+            //public static int SumMoreFive(CheckNums fact, int[] nums)
+            //{
+            //    var sum = 0;
+
+            //    foreach (var item in nums)
+            //    {
+            //        if (item>5)
+            //        {
+            //            sum += item;
+            //        }
+            //    }
+            //    return sum;
+
+
+
+        //}
+
+    }
+}
